2468e9764022bfd968fec5d1eabaf516cacd411a
DESKTOP-FPTSOG5\DarkTuan <bachdangtuan93@gmail.com>
2023-02-26 22:02:01 +0700

    test biến hệ thống

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Biến hệ thống
+
+# shellcheck disable=SC2034
+arrayMenu=("Cài đặt dịch vụ service Linux" "Cài đặt New Server " "Check systems basic" "Thoát")
--- menu-script.sh
+++ menu-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Import variables from config.sh
+source ./config.sh
 
+echo "$arrayMenu"
 # Variable Array Menu list level 1
 menu=("Cài đặt dịch vụ service Linux" "Cài đặt New Server " "Check systems basic" "Thoát")
 
@@ -8,54 +11,50 @@ menuService=("Prometheus" "Prometheus Exporter" "Grafana" "Zabbix" "Zabbix Agent
 
 # ################################
 
-
-
-
 # Function list menu level1
-print_menu_level1 () {
-for (( i=0; i<${#menu[@]}; i++ )); do
-    echo "                             $((i+1)). ${menu[$i]}                          "
-done
+print_menu_level1() {
+  for ((i = 0; i < ${#menu[@]}; i++)); do
+    echo "                             $((i + 1)). ${menu[$i]}                          "
+  done
 }
 
-
 # Function list menu level2
-print_menu_level2 () {
-        for (( i=0; i<${#menuService[@]}; i++ )); do
-        echo "                             $((i+1)). ${menuService[$i]}                          "
-        done
-        
-        read -p "Vui lòng chọn một lựa chọn dịch vụ montor: " choice
-        case $choice in
-    1)
-        echo "Cài đặt ${menuService[0]} "
-        source ./setup/prometheus-grafana/install-prometheus.sh
+print_menu_level2() {
+  for ((i = 0; i < ${#menuService[@]}; i++)); do
+    echo "                             $((i + 1)). ${menuService[$i]}                          "
+  done
 
-        ;;
-    2)
-        echo "Cài đặt ${menuService[1]} "
-        source ./setup/prometheus-grafana/install-prometheus-exporter.sh
-        ;;
-    3)
-        echo "Cài đặt ${menuService[2]} "
-        source ./setup/zabbis/install-zabbix.sh
-        ;;
+  read -p "Vui lòng chọn một lựa chọn dịch vụ montor: " choice
+  case $choice in
+  1)
+    echo "Cài đặt ${menuService[0]} "
+    source ./setup/prometheus-grafana/install-prometheus.sh
 
-    4)
-        echo "Cài đặt ${menuService[3]} "
-        ;;    
+    ;;
+  2)
+    echo "Cài đặt ${menuService[1]} "
+    source ./setup/prometheus-grafana/install-prometheus-exporter.sh
+    ;;
+  3)
+    echo "Cài đặt ${menuService[2]} "
+    source ./setup/zabbis/install-zabbix.sh
+    ;;
 
-    5)
-        echo "Cài đặt ${menuService[4]} "
-        ;;
+  4)
+    echo "Cài đặt ${menuService[3]} "
+    ;;
 
-    6)
-        echo "Thank you, See again !"
-        ;;
-    *)
-        echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
-        ;;
-esac      
+  5)
+    echo "Cài đặt ${menuService[4]} "
+    ;;
+
+  6)
+    echo "Thank you, See again !"
+    ;;
+  *)
+    echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
+    ;;
+  esac
 }
 
 # Giao diện hiển thị
@@ -68,32 +67,30 @@ echo "####                               CREATE BY DangTuan                     
 echo "####                                   version 1.0                           ####"
 echo "--------------------------------------------------------------------------------"
 
-
 print_menu_level1
-
 
 echo "--------------------------------------------------------------------------------"
 echo "##                                                                            ##"
 echo "#####                                                                     ######"
 echo "################################################################################"
 echo "################################################################################"
-read -p "Vui lòng chọn một lựa chọn [1-3]: " choice
+read -p "Vui lòng chọn một lựa chọn [1-4]: " choice
 
 case $choice in
-    1)
-        print_menu_level2
-        ;;
-    2)
-        echo "Hệ thống cài đặt máy chủ mới bắt đầu......."
-        source ./setup/setup-new-server/setup-new-server.sh
-        ;;
-    3)
-        exit
-        ;;
-    *)
-        echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
-        ;;
+1)
+  print_menu_level2
+  ;;
+2)
+  echo "Hệ thống cài đặt máy chủ mới bắt đầu......."
+  source ./setup/setup-new-server/setup-new-server.sh
+  ;;
+3)
+  echo "Chương trình hệ thống......."
+  ;;
+4)
+  exit
+  ;;
+*)
+  echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
+  ;;
 esac
-
-
-
